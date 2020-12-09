79ef66414ca204e941e4461b0e68cf615032b703
Ashwin <bsashwin9@gmail.com>
2020-12-09 20:29:51 +0530

    Initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,12 +1,10 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {AppPage} from './app.po';
+import {browser, logging} from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+  beforeEach(() => page = new AppPage());
 
   it('should display welcome message', async () => {
     await page.navigateTo();
